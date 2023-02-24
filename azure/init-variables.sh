d792210b983f5d03ca447d1174f7bafc6758545f
Jaime Still <jpstill85@gmail.com>
2023-02-24 14:02:44 -0500

    verify and fix scripts

---
+++ azure/init-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Initialized Variables
+export rg=arma-demo-rg
+export kv=arma-demo-vault
+export acr=armaregistry
+export appPlan=demo-app-plan
+export api1=jps-primary-api
+export api2=jps-secondary-api
+export hook1=deployprimary
+export hook2=deploysecondary
+export adApp=demo-api
+export scopeId=$(uuidgen)
