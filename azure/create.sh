2fffeb424a36cc48dc2c038921be6a948cc9fd7e
Jaime Still <jpstill85@gmail.com>
2023-02-24 13:12:05 -0500

    containerized apps + key vault

--- azure/create.sh
+++ azure/create.sh
@@ -4,8 +4,9 @@ kv=arma-demo-vault
 acr=armaregistry
 ad-app=demo-api
 scopeId=$(uuidgen)
-api-1=primary-api
-api-2=secondary-api
+appPlan=demo-app-plan
+api1=primary-api
+api2=secondary-api
 
 
 # Resource Group
@@ -33,18 +34,79 @@ ACR_PW=$(az acr credential show \
     --output tsv)
 
 # Build Docker images with ACR
+az acr build \
+    --registry $acr \
+    --image $api1 \
+    ../src/$api1
 
 az acr build \
     --registry $acr \
-    --image $api-1 \
-    ../src/$api-1
+    --image $api2 \
+    ../src/$api2
 
-az acr build \
+# App Plan
+az appservice plan create \
+    --name $appPlan \
+    --resource-group $rg \
+    --sku F1 \
+    --is-linux
+
+## App Services
+az webapp create \
+    --name $app1 \
+    --plan $appPlan \
+    --resource-group $rg \
+    --docker-registry-server-user $ACR_ADMIN \
+    --docker-registry-server-password $ACR_PW \
+    --deployment-container-image-name $acr.azureacr.io/$app1:latest
+
+az webapp create \
+    --name $app2 \
+    --plan $appPlan \
+    --resource-group $rg \
+    --docker-registry-server-user $ACR_ADMIN \
+    --docker-registry-server-password $ACR_PW \
+    --deployment-container-image-name $acr.azureacr.io/$app2:latest
+
+# Configure Continuous Deployment
+az webapp deployment container config \
+    --resoruce-group $rg \
+    --name $app1 \
+    --enable-cd true
+
+az webapp deployment container config \
+    --resource-group $rg \
+    --name $app2 \
+    --enable-cd true
+
+# Configure ACR CD Webhooks
+PRIMARY_WEBHOOK=$(az webapp deployment container show-cd-url \
+    --resource-group $rg \
+    --name $app1 \
+    --query "CI_CD_URL" \
+    --output tsv
+)
+
+SECONDARY_WEBHOOK=$(az webapp deployment container show-cd-url \
+    --resource-group $rg \
+    --name $app2 \
+    --query "CI_CD_URL" \
+    --output tsv
+)
+
+az acr webook create \
+    --resource-group $rg \
+    --regisry $acr \
+    --name deploy-$app1 \
+    --uri $PRIMARY_WEBHOOK \
+    --actions push
+
+az acr webhook create \
+    --resource-group $rg \
     --registry $acr \
-    --image $api-2 \
-    ../src/$api-2
-
-# App Service
+    --name deploy-$app2 \
+    --uri $SECONDARY_WEBHOOK \
+    --actions push
 
 # Key Vault
 az keyvault create \
@@ -56,6 +118,61 @@ az keyvault secret set \
     --name DatabaseConnection \
     --value "Server=nerual.local;database=neural-db;UID=sa;PWD=P@$$Word1234!@#$;" \
     --vault-name $kv
+
+# Add Key Vault to App Service settings
+
+az webapp config appsettings set \
+    --resource-group $rg \
+    --name $app1 \
+    --settings "VaultName=$kv"
+
+az webapp config appsettings set \
+    --resource-group $rg \
+    --name $app2 \
+    --settings "VaultName=$kv"
+
+# Enable Web App Managed Identity
+az webapp identity assign \
+    --resource-group $rg \
+    --name $app1
+
+az webapp identity assign \
+    --resource-group $rg \
+    --name $app2
+
+# Grant Key Vault Access
+
+PRIMARY_PRINCIPAL=$(az webapp identity show \
+    --resource-group $rg \
+    --name $app1 \
+    --query principalId \
+    --output tsv)
+
+SECONDARY_PRINCIPAL=$(az webapp identity show \
+    --resource-group $rg \
+    --name $app2 \
+    --query principalId \
+    --output tsv)
+
+az keyvault set-policy \
+    --name $kv \
+    --secret-permissions get list \
+    --object-id $PRIMARY_PRINCIPAL
+
+az keyvault set-policy \
+    --name $kv \
+    --secret-permissions get list \
+    --object-id $SECONDARY_PRINCIPAL
+
+# Add Key Vault config to appsettings in APIs
+
+jq '. += {
+    "VaultName": "'$kv'"
+}' ../src/$api1/appsettings.json > "tmp" && mv "tmp" ../src/$api1/appsettings.json
+
+jq '. += {
+    "VaultName": "'$kv'"
+}' ../src/$api2/appsettings.json > "tmp" && mv "tmp" ../src/$api2/appsettings.json
 
 # Configure AD App Registration
 appId=$(az ad app create \
@@ -95,11 +212,11 @@ jq '.AzureAd += {
     "Domain": "qualified.domain.name",
     "ClientId": "$appId",
     "TenantId": "common"
-}' ../src/$api-1/appsettings.json > "tmp" && mv "tmp" ../src/$api-1/appsettings.json
+}' ../src/$api1/appsettings.json > "tmp" && mv "tmp" ../src/$api1/appsettings.json
 
 jq '.AzureAd += {
     "Instance": "https://login.microsoftonline.com/",
     "Domain": "qualified.domain.name",
     "ClientId": "$appId",
     "TenantId": "common"
-}' ../src/$api-2/appsettings.json > "tmp" && mv "tmp" ../src/$api-2/appsettings.json
+}' ../src/$api2/appsettings.json > "tmp" && mv "tmp" ../src/$api2/appsettings.json
